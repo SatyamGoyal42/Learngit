074325330df6127ec52528a6671ff3fb5f31a6d0
Satyam Goyal <satyam1goyal1@gmail.com>
2023-06-12 13:08:02 +0530

    after commit

--- fit_git.cpp
+++ fit_git.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main()
 {
-    cout<<"FIRSt GIT";
+    cout<<"FIRStGIT";
 
 }
---
+++ hello.cpp
@@ -0,0 +1,5 @@
+[33mcommit e956e572136b52602ca9efdc29e2a7d13d3eab31[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Satyam Goyal <satyam1goyal1@gmail.com>
+Date:   Mon Jun 12 12:46:53 2023 +0530
+
+    intial commit
